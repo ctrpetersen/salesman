ad9dc0c2ea64bf7f978beb0975fc14b1a4cc5a4d
Christoffer Petersen <ctrpetersen@outlook.com>
2018-10-11 00:51:53 +0200

    kermit

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,67 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+var numberOfCities: Number = Number((<HTMLInputElement>document.getElementById("citiesInput")).value);
+var canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("mainCanvas");
+var context: CanvasRenderingContext2D = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 800;
+
+class City{
+    constructor(x:number, y:number){
+        this.x = x;
+        this.y = y;
+    }
+
+    public x:number = 0;
+    public y:number = 0;
+    
+    public cityOne:City = null;
+    public cityTwo:City = null;
+
+    Draw() {
+        context.fillRect(this.x,this.y,5,5)
+    }
+
+    DrawLines() {
+        
+    }
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+var cities:City[] = new Array<City>();
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+for (let index = 0; index < numberOfCities; index++) {
+    cities.push(new City(getRandomInt(0, canvas.width-10), getRandomInt(0, canvas.height-10)))
+}
+
+for (let index = 0; index < numberOfCities; index++) {
+    cities[index].cityOne = cities[getRandomInt(0,cities.length)];
+
+    var temp = getRandomInt(0,cities.length);
+    while (cities[index].cityOne !== cities[temp]){
+        temp = getRandomInt(0,cities.length);
+    }
+    cities[index].cityTwo = cities[temp];
+    
+}
+
+console.log(cities.length);
+
+
+function MainLoop() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    window.requestAnimationFrame(MainLoop.bind(this));
+
+    cities.forEach(element => {
+        element.Draw();
+    });
+
+
+}
+
+function getRandomInt(min:number, max:number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+MainLoop();
